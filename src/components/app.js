b701f20c2b085e77861f86befef92070831bb14d
goudeaulopez <goudeaulopez@gmail.com>
2020-12-07 20:10:24 +1100

    adding compone

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
 class App extends React.Component{
     render(){
         return(
             <div>
+                <GoogleReCaptchaProvider>
                 App component
+                </GoogleReCaptchaProvider>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ ReactDOM.render(
     <App/>,
     document.querySelector('#root')
 )
+
+
+//siteKey:6Lc8sfwZAAAAANA6K56DbT7FI8xcav18UoDZamcK
+//secretKey:6Lc8sfwZAAAAANuFgD3GVX5lRVd0RYHCak8KPTPM
