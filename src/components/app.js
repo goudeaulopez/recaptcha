de83993a96c37ff6d157e33db8fc67c4f875d57c
goudeaulopez <goudeaulopez@gmail.com>
2020-12-07 20:02:45 +1100

    adding compone

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                App component
+            </div>
+        )
+    }
+}
+export default App
